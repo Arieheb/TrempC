2c45f574e7a84f1cdcc4b2e644ed51ff9b1ca096
Arieh Berlin <99349871+Arieheb@users.noreply.github.com>
2024-07-23 17:43:53 +0300

    Forgot password complete

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,96 +1,103 @@
-import React, {useState, useEffect} from 'react';
-import { View, Text, SafeAreaView, StyleSheet, Button, TextInput, Image, ScrollView, KeyboardAvoidingView, useWindowDimensions } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, SafeAreaView, StyleSheet, Button, TextInput, Image, ScrollView, KeyboardAvoidingView, useWindowDimensions, Alert } from 'react-native';
 import { CommonActions, useNavigation } from '@react-navigation/native';
-import {db, auth} from '../../../firebaseConfig';
-import { addDoc, collection } from 'firebase/firestore';
-import { signInWithEmailAndPassword } from 'firebase/auth';
-import { Alert } from 'react-native';
+import { db, auth } from '../../../firebaseConfig';
+import { doc, getDoc, updateDoc } from 'firebase/firestore';
+import { signInWithEmailAndPassword, updatePassword } from 'firebase/auth';
 
-
-import Photo from '../../../assets/images/logo.png'
+import Photo from '../../../assets/images/logo.png';
 import CustomInput from '../../components/CustomInput';
 import CustomButton from '../../components/CustomButton';
-import SocialSignInButtons from '../../components/SocialSignInButtons';
 
-
-const Login = props => {
+const Login = (props) => {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
-    const {height} = useWindowDimensions();
+    const { height } = useWindowDimensions();
     const navigation = useNavigation();
-
 
     const onForgotPasswordPress = () => {
         navigation.navigate('forgotPasswordScreen');
     }
 
-
     const signUpPress = () => {
         navigation.navigate('signUpScreen');
     }
 
-
-    const handleSignIn = () => {   
-        auth
-        signInWithEmailAndPassword(auth, username, password)
-        
-        .then((userCredential) => {
+    const handleSignIn = async () => {
+        try {
+            const userCredential = await signInWithEmailAndPassword(auth, username, password);
             const user = userCredential.user;
-            navigation.dispatch(CommonActions.reset({
-                index: 0,
-                routes: [
-                    { name: 'homeScreen' }
-                ],
-            })
-            )
-            console.log(user.uid)
-          })
-          
-          .catch(error => {
-            Alert.alert('Error', error.message);           
-            
-          }
-        );
-          
+
+            // Check if passHasChanged is true
+            const userRef = doc(db, 'users', user.uid);
+            const userDoc = await getDoc(userRef);
+
+            if (userDoc.exists() && userDoc.data().passHasChanged) {
+                if (userDoc.data().password === password) {
+                    // Set passHasChanged to false
+                    await updateDoc(userRef, {
+                        passHasChanged: false,
+                    });
+                } else {
+                    // Update the password and set passHasChanged to false
+                    await updatePassword(user, password);
+                    await updateDoc(userRef, {
+                        passHasChanged: false,
+                        password: password
+                    });
+                }
+            }
+
+            navigation.dispatch(
+                CommonActions.reset({
+                    index: 0,
+                    routes: [{ name: 'homeScreen' }],
+                })
+            );
+            console.log(user.uid);
+        } catch (error) {
+            Alert.alert('Error', error.message);
+        }
     }
 
     return (
-        <ScrollView style={{backgroundColor: 'white'}}>
-            <KeyboardAvoidingView style={styles.container}>            
-                <Image 
-                    source={Photo} 
-                    style={[styles.logo, {height:height*0.3}]} 
-                    resizeMode="contain" 
+        <ScrollView style={{ backgroundColor: 'white' }}>
+            <KeyboardAvoidingView style={styles.container}>
+                <Image
+                    source={Photo}
+                    style={[styles.logo, { height: height * 0.3 }]}
+                    resizeMode="contain"
                 />
-                
-                <CustomInput 
-                    placeholder="Username" 
-                    value={username} 
-                    setValue={setUsername} />
-                
-                <CustomInput 
-                    placeholder="Password" 
-                    value={password} 
+
+                <CustomInput
+                    placeholder="Username"
+                    value={username}
+                    setValue={setUsername}
+                />
+
+                <CustomInput
+                    placeholder="Password"
+                    value={password}
                     setValue={setPassword}
                     secureTextEntry
-                     />
+                />
 
-                <CustomButton 
-                    text = "Sign In" 
-                    onPress={handleSignIn}                 
-                    />
+                <CustomButton
+                    text="Sign In"
+                    onPress={handleSignIn}
+                />
 
-                <CustomButton 
-                    text = "Forgot Password?" 
-                    onPress={onForgotPasswordPress} 
-                    type = "TERTIARY"/>
-                
-                <CustomButton 
-                    text = "Don't have an account? Sign Up" 
-                    onPress={signUpPress} 
-                    type = "TERTIARY"/>
+                <CustomButton
+                    text="Forgot Password?"
+                    onPress={onForgotPasswordPress}
+                    type="TERTIARY"
+                />
 
-                
+                <CustomButton
+                    text="Don't have an account? Sign Up"
+                    onPress={signUpPress}
+                    type="TERTIARY"
+                />
             </KeyboardAvoidingView>
         </ScrollView>
     );
@@ -102,7 +109,6 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
         backgroundColor: '#fff',
-        
     },
     inputView: {
         backgroundColor: 'white',
@@ -110,9 +116,8 @@ const styles = StyleSheet.create({
         borderColor: 'black',
         borderWidth: 1,
         borderRadius: 10,
-
         paddingHorizontal: 10,
-        marginVertical: 5,        
+        marginVertical: 5,
     },
     input: {
         width: '100%',
@@ -124,7 +129,138 @@ const styles = StyleSheet.create({
         width: '70%',
         maxWidth: 300,
         maxHeight: 200,
-    }
+    },
 });
 
 export default Login;
+
+// import React, {useState, useEffect} from 'react';
+// import { View, Text, SafeAreaView, StyleSheet, Button, TextInput, Image, ScrollView, KeyboardAvoidingView, useWindowDimensions } from 'react-native';
+// import { CommonActions, useNavigation } from '@react-navigation/native';
+// import {db, auth} from '../../../firebaseConfig';
+// import { addDoc, collection } from 'firebase/firestore';
+// import { signInWithEmailAndPassword } from 'firebase/auth';
+// import { Alert } from 'react-native';
+
+
+// import Photo from '../../../assets/images/logo.png'
+// import CustomInput from '../../components/CustomInput';
+// import CustomButton from '../../components/CustomButton';
+// import SocialSignInButtons from '../../components/SocialSignInButtons';
+
+
+// const Login = props => {
+//     const [username, setUsername] = useState("");
+//     const [password, setPassword] = useState("");
+//     const {height} = useWindowDimensions();
+//     const navigation = useNavigation();
+
+
+//     const onForgotPasswordPress = () => {
+//         navigation.navigate('forgotPasswordScreen');
+//     }
+
+
+//     const signUpPress = () => {
+//         navigation.navigate('signUpScreen');
+//     }
+
+
+//     const handleSignIn = () => {   
+//         auth
+//         signInWithEmailAndPassword(auth, username, password)
+        
+//         .then((userCredential) => {
+//             const user = userCredential.user;
+//             navigation.dispatch(CommonActions.reset({
+//                 index: 0,
+//                 routes: [
+//                     { name: 'homeScreen' }
+//                 ],
+//             })
+//             )
+//             console.log(user.uid)
+//           })
+          
+//           .catch(error => {
+//             Alert.alert('Error', error.message);           
+            
+//           }
+//         );
+          
+//     }
+
+//     return (
+//         <ScrollView style={{backgroundColor: 'white'}}>
+//             <KeyboardAvoidingView style={styles.container}>            
+//                 <Image 
+//                     source={Photo} 
+//                     style={[styles.logo, {height:height*0.3}]} 
+//                     resizeMode="contain" 
+//                 />
+                
+//                 <CustomInput 
+//                     placeholder="Username" 
+//                     value={username} 
+//                     setValue={setUsername} />
+                
+//                 <CustomInput 
+//                     placeholder="Password" 
+//                     value={password} 
+//                     setValue={setPassword}
+//                     secureTextEntry
+//                      />
+
+//                 <CustomButton 
+//                     text = "Sign In" 
+//                     onPress={handleSignIn}                 
+//                     />
+
+//                 <CustomButton 
+//                     text = "Forgot Password?" 
+//                     onPress={onForgotPasswordPress} 
+//                     type = "TERTIARY"/>
+                
+//                 <CustomButton 
+//                     text = "Don't have an account? Sign Up" 
+//                     onPress={signUpPress} 
+//                     type = "TERTIARY"/>
+
+                
+//             </KeyboardAvoidingView>
+//         </ScrollView>
+//     );
+// };
+
+// const styles = StyleSheet.create({
+//     container: {
+//         flex: 1,
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         backgroundColor: '#fff',
+        
+//     },
+//     inputView: {
+//         backgroundColor: 'white',
+//         width: '95%',
+//         borderColor: 'black',
+//         borderWidth: 1,
+//         borderRadius: 10,
+
+//         paddingHorizontal: 10,
+//         marginVertical: 5,        
+//     },
+//     input: {
+//         width: '100%',
+//         display: 'flex',
+//         alignItems: 'center',
+//         justifyContent: 'center',
+//     },
+//     logo: {
+//         width: '70%',
+//         maxWidth: 300,
+//         maxHeight: 200,
+//     }
+// });
+
+// export default Login;
--- src/pages/NewPassword/NewPassword.js
+++ src/pages/NewPassword/NewPassword.js
@@ -1,6 +1,8 @@
-import React, {useState, useEffect} from 'react';
-import { View, Text, SafeAreaView, StyleSheet, Button, TextInput, ScrollView, KeyboardAvoidingView, useWindowDimensions } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
+import React, { useState } from 'react';
+import { View, Text, SafeAreaView, StyleSheet, ScrollView, KeyboardAvoidingView, Alert } from 'react-native';
+import { useNavigation, useRoute } from '@react-navigation/native';
+import { getFirestore, collection, query, where, getDocs, doc, updateDoc } from 'firebase/firestore';
+import { getAuth, updatePassword } from 'firebase/auth';
 
 import CustomInput from '../../components/CustomInput';
 import CustomButton from '../../components/CustomButton';
@@ -8,50 +10,81 @@ import CustomButton from '../../components/CustomButton';
 const NewPassword = () => {
     const [code, setCode] = useState('');
     const [newPassword, setNewPassword] = useState('');
-    const {height} = useWindowDimensions();
+    const [confirmCode, setConfirmCode] = useState('');
     const navigation = useNavigation();
+    const route = useRoute();
+    const { email } = route.params;
+    const db = getFirestore();
+    const auth = getAuth();
 
+    const onSubmitPress = async () => {
+        try {
+            const q = query(collection(db, 'users'), where('email', '==', email));
+            const querySnapshot = await getDocs(q);
+            if (!querySnapshot.empty) {
+                const userDoc = querySnapshot.docs[0];
+                const userData = userDoc.data();
 
+                if (userData.confirmationCode === confirmCode) {
+                    // Update the password in Firebase Authentication
+                    const user = await auth.getUserByEmail(email);
+                    await updatePassword(user, newPassword);
 
-    const navigateToLogin = () => {
+                    // Update the password in Firestore
+                    await updateDoc(doc(db, 'users', userDoc.id), { password: newPassword });
+
+                    Alert.alert('Success', 'Password has been reset. Please sign in with your new password.');
+                    navigation.navigate('loginScreen');
+                } else {
+                    Alert.alert('Error', 'Confirmation code does not match.');
+                }
+            } else {
+                Alert.alert('Error', 'No user found with this email address.');
+            }
+        } catch (error) {
+            console.log('Error resetting password:', error);
+            Alert.alert('Error', 'An error occurred while resetting the password.');
+        }
+    };
+
+    const onSignInPress = () => {
         navigation.navigate('loginScreen');
     };
 
-    
-    const navigateToHome = () => {
-        navigation.navigate('homeScreen');
-    };
-
     return (
-        <ScrollView style={{backgroundColor: 'white'}}>
+        <ScrollView style={{ backgroundColor: 'white' }}>
             <KeyboardAvoidingView style={styles.container}>
                 <Text style={styles.title}>Reset Your Password</Text>
                 
-                <CustomInput 
-                    placeholder="Enter Confirmation Code" 
-                    value={code} 
-                    setValue={setCode}
-                    />
+                <CustomInput
+                    placeholder="Enter Confirmation Code"
+                    value={confirmCode}
+                    setValue={setConfirmCode} />
 
                 <CustomInput 
                     placeholder="Enter New Password" 
                     value={newPassword} 
                     setValue={setNewPassword}
                     secureTextEntry
-                     />
-
+                />
+            
+                <CustomInput 
+                    placeholder="Confirm new password" 
+                    value={code} 
+                    setValue={setCode}
+                    secureTextEntry
+                />
 
                 <CustomButton
-                    text = "Submit"
-                    onPress={navigateToHome}
-                     />
+                    text="Submit"
+                    onPress={onSubmitPress}
+                />
 
                 <CustomButton 
-                    text = "Back To Sign In" 
-                    onPress={navigateToLogin} 
-                    type = "TERTIARY"
-
-                    />
+                    text="Back To Sign In" 
+                    onPress={onSignInPress} 
+                    type="TERTIARY"
+                />
                 
             </KeyboardAvoidingView>
         </ScrollView>
@@ -73,10 +106,8 @@ const styles = StyleSheet.create({
     },
     text: {
         color: "gray",
-        // margin: 10,
         padding: 5,
         marginVertical: 10,
-    
     },
     link: {
         color: 'blue',
@@ -84,3 +115,99 @@ const styles = StyleSheet.create({
 });
 
 export default NewPassword;
+
+// import React, {useState, useEffect} from 'react';
+// import { View, Text, SafeAreaView, StyleSheet, Button, TextInput, ScrollView, KeyboardAvoidingView, useWindowDimensions } from 'react-native';
+// import { useNavigation } from '@react-navigation/native';
+
+// import CustomInput from '../../components/CustomInput';
+// import CustomButton from '../../components/CustomButton';
+
+// const NewPassword = () => {
+//     const [code, setCode] = useState('');
+//     const [newPassword, setNewPassword] = useState('');
+//     const [confirmCode, setConfirmCode] = useState('');
+//     const {height} = useWindowDimensions();
+//     const navigation = useNavigation();
+
+
+
+//     const navigateToLogin = () => {
+//         navigation.navigate('loginScreen');
+//     };
+
+    
+//     const navigateToHome = () => {
+//         navigation.navigate('homeScreen');
+//     };
+
+//     return (
+//         <ScrollView style={{backgroundColor: 'white'}}>
+//             <KeyboardAvoidingView style={styles.container}>
+//                 <Text style={styles.title}>Reset Your Password</Text>
+                
+//                 <CustomInput
+//                     placeholder="Enter Confirmation Code"
+//                     value={confirmCode}
+//                     setValue={setConfirmCode} />
+
+//                 <CustomInput 
+//                     placeholder="Enter New Password" 
+//                     value={newPassword} 
+//                     setValue={setNewPassword}
+//                     secureTextEntry
+//                      />
+            
+//                 <CustomInput 
+//                     placeholder="Confirm new password" 
+//                     value={code} 
+//                     setValue={setCode}
+//                     />
+
+//                 <CustomButton
+//                     text = "Submit"
+//                     onPress={navigateToHome}
+//                      />
+
+//                 <CustomButton 
+//                     text = "Back To Sign In" 
+//                     onPress={navigateToLogin} 
+//                     type = "TERTIARY"
+
+//                     />
+                
+//             </KeyboardAvoidingView>
+//         </ScrollView>
+//     );
+// };
+
+
+// // lets start fresh. the last update of the forgot password is working (checking and comparing that the emails match). upon success, send the user an email with a confirmation code. 
+// // in the newPassword screen, the user will insert the confirmation code into a confirmation field, and then create the new password. 
+
+// const styles = StyleSheet.create({
+//     container: {
+//         flex: 1,
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         backgroundColor: '#fff',
+//     },
+//     title: {
+//         fontSize: 30,
+//         fontWeight: 'bold',
+//         margin: 15,
+//         padding: 15,
+//     },
+//     text: {
+//         color: "gray",
+//         // margin: 10,
+//         padding: 5,
+//         marginVertical: 10,
+    
+//     },
+//     link: {
+//         color: 'blue',
+//     }
+// });
+
+// export default NewPassword;
